ddb38a691af965829f2b742ce7c6c3682573b7b8
Ivan Petrov <51787774+IvanPetrovISP@users.noreply.github.com>
2020-01-25 13:39:57 +0200

    Update Queries_soft_uni_database.sql
    Added exercise #17

--- 01. Exercises/03. Data Aggregation/Queries_soft_uni_database.sql
+++ 01. Exercises/03. Data Aggregation/Queries_soft_uni_database.sql
@@ -28,15 +28,24 @@ WHERE `manager_id` IS NULL
 GROUP BY `manager_id`;
 
 #17. 3rd Highest Salary
-#soonTM
+SELECT `department_id`,
+    (SELECT DISTINCT `e2`.`salary`
+        FROM `employees` AS `e2`
+        WHERE `e2`.`department_id` = `e1`.`department_id`
+        ORDER BY `e2`.`salary` DESC
+        LIMIT 2 , 1) AS `third_highest_salary`
+FROM `employees` AS `e1`
+GROUP BY `department_id`
+HAVING `third_highest_salary` IS NOT NULL
+ORDER BY `department_id`;
 
 #18. Salary Challenge
-SELECT e.`first_name`, e.`last_name`, e.`department_id` FROM `employees` AS `e`
+SELECT e1.`first_name`, e1.`last_name`, e1.`department_id` FROM `employees` AS `e1`
 WHERE `salary` > (
     SELECT avg(e2.`salary`) FROM `employees` AS  `e2`
-    WHERE e2.`department_id` = e.`department_id`
+    WHERE e2.`department_id` = e1.`department_id`
     GROUP BY e2.`department_id`)
-ORDER BY e.`department_id`, e.`employee_id`
+ORDER BY e1.`department_id`, e1.`employee_id`
 LIMIT 10;
 
 #19. Departments Total Salaries
