4ed98fdd74dab7298d71b76ba9dd6f68c54b7934
Ivan Petrov <51787774+IvanPetrovISP@users.noreply.github.com>
2020-02-02 15:01:19 +0200

    Update exam_february_2018.sql

--- 03. Exams/03. 25-February-2018/exam_february_2018.sql
+++ 03. Exams/03. 25-February-2018/exam_february_2018.sql
@@ -75,14 +75,48 @@ CREATE TABLE `files` (
 
 #Section 2: Data Manipulation Language (DML)
 #02. Insert
+INSERT INTO `issues` (`title`, `issue_status`, `repository_id`, `assignee_id`)
+SELECT concat('Critical Problem With ', f.`name`, '!'),
+       'open', ceil((f.`id` * 2) / 3), c.`contributor_id`
+FROM `files` as `f`
+JOIN `commits` `c` ON `f`.`commit_id` = `c`.`id`
+WHERE f.`id` >= 46 and f.`id` <=50;
+
 #03. Update
 #04. Delete
+DELETE FROM `repositories`
+WHERE `id` NOT IN (SELECT i.`repository_id` FROM `issues` as `i` );
+
 #Section 3: Querying
 #05. Users
+SELECT `id`, `username`
+FROM `users`
+ORDER BY `id`;
+
 #06. Lucky Numbers
+SELECT `repository_id`, `contributor_id`
+FROM `repositories_contributors`
+WHERE `repository_id` = `contributor_id`
+ORDER BY repository_id;
+
 #07. Heavy HTML
+SELECT id, `name`, `size`
+FROM `files`
+WHERE `size` > 1000 AND name LIKE '%html%'
+ORDER BY `size` DESC;
+
 #08. IssuesAndUsers
+SELECT i.`id`, concat(u.`username`, ' : ', i.`title`) AS `issue_assignee`
+FROM `issues` AS `i`
+JOIN `users` `u` ON `i`.`assignee_id` = `u`.`id`
+ORDER BY i.id DESC;
+
 #09. NonDirectoryFiles
+SELECT f.`id`, f.`name` as `Name`, concat(f.`size`, 'KB') as `size` FROM `files` AS `f`
+LEFT JOIN `files` `f2` ON f.`id` = `f2`.`parent_id`
+WHERE f2.`parent_id` IS NULL
+ORDER BY f.`id`;
+
 #10. ActiveRepositories
 #11. MostContributedRepository
 #12. FixingMyOwnProblems
