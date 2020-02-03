7990220184545d7d377457e0db775613af4fc021
Ivan Petrov <51787774+IvanPetrovISP@users.noreply.github.com>
2020-02-03 15:30:29 +0200

    Update exam_september_2017.sql

--- 03. Exams/04. 03-September-2017/exam_september_2017.sql
+++ 03. Exams/04. 03-September-2017/exam_september_2017.sql
@@ -129,6 +129,15 @@ ORDER BY `comments` DESC, p.`id`
 LIMIT 5;
 
 #11. Most Popular User
+SELECT u.`id`, u.`username`, COUNT(u.id) AS `posts`,
+    (SELECT COUNT(uf.`follower_id`)
+    FROM `users_followers` AS `uf`
+	WHERE u.`id` = uf.`user_id`) AS `followers`
+FROM `users` AS `u`
+JOIN `posts` AS `p` ON `p`.`user_id` = `u`.`id`
+GROUP BY u.`id`
+ORDER BY followers DESC
+LIMIT 1;
 
 #Section 4: Programmability
 
