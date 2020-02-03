08055b8cbd93c01e0cd18086714159f8f095972f
Ivan Petrov <51787774+IvanPetrovISP@users.noreply.github.com>
2020-02-03 17:40:46 +0200

    Update exam_september_2017.sql

--- 03. Exams/04. 03-September-2017/exam_september_2017.sql
+++ 03. Exams/04. 03-September-2017/exam_september_2017.sql
@@ -164,7 +164,41 @@ ORDER BY `users` DESC, p.`id`;
 
 #Section 4: Programmability
 #15. Post
+DELIMITER ;;
+CREATE PROCEDURE `udp_post` (`username` VARCHAR(255), `password` VARCHAR(255), `caption` VARCHAR(255), `path` VARCHAR(255))
+BEGIN
+    IF ((SELECT u.`password` FROM `users` AS `u`
+        WHERE u.`username` = `username`) != `password`)
+    THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Password is incorrect!';
+    ELSEIF ((SELECT p.`path` FROM `pictures` AS `p`
+        WHERE p.`path` = `path`) IS NULL )
+    THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'The picture does not exist!';
+    ELSE
+        INSERT INTO `posts` (`caption`, `user_id`, `picture_id`)
+        SELECT `caption`,
+               (SELECT u.`id` FROM `users` AS `u`
+               WHERE u.`username` = `username`) AS `user_id`,
+               (SELECT p.`id` FROM `pictures` AS `p`
+                   WHERE p.`path` = `path`) AS `picture_id`;
+    END IF ;
+END ;;
+DELIMITER ;
+
 #16. Filter
+DELIMITER ;;
+CREATE PROCEDURE `udp_filter` (`hashtag` VARCHAR(255))
+BEGIN
+    SELECT p.`id`, p.`caption`, u.`username`
+    FROM `posts` AS `p`
+    JOIN `users` `u` ON `p`.`user_id` = `u`.`id`
+    WHERE p.`caption` LIKE (concat('%', `hashtag`, '%'))
+    ORDER BY p.`id`;
+END ;;
+DELIMITER ;
 
 /*
  The SoftUni Open Judge System does not accept the
