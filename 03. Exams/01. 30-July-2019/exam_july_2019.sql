6837a6f6791e9b8212d3b32b6ea73cfdecfa3bf2
Ivan Petrov <51787774+IvanPetrovISP@users.noreply.github.com>
2020-02-05 00:55:10 +0200

    Update exam_july_2019.sql

--- 03. Exams/01. 30-July-2019/exam_july_2019.sql
+++ 03. Exams/01. 30-July-2019/exam_july_2019.sql
@@ -78,7 +78,21 @@ FROM `users` AS `u`
 WHERE u.`id` BETWEEN 16 AND 20;
 
 #03. Update
+UPDATE `comments` AS `c`
+SET `c`.`comment` =
+    (CASE
+        WHEN c.`id` % 2 = 0 THEN 'Very good article.'
+        WHEN c.`id` % 3 = 0 THEN 'This is interesting.'
+        WHEN c.`id` % 5 = 0 THEN 'I definitely will read the article again.'
+        WHEN c.`id` % 7 = 0 THEN 'The universe is such an amazing thing.'
+        ELSE c.`comment`
+    END)
+WHERE c.`id` BETWEEN 1 AND 15;
+
 #04. Delete
+DELETE FROM `articles`
+WHERE `category_id` IS NULL;
+
 #Section 3: Querying
 #05. Extract 3 biggest articles
 #06. Golden articles
