304399f8ad780b9fea4c1eb6fe29a0a9857fc57a
Ivan Petrov <51787774+IvanPetrovISP@users.noreply.github.com>
2020-02-05 00:20:45 +0200

    Create exam_july_2019.sql

---
+++ 03. Exams/01. 30-July-2019/exam_july_2019.sql
@@ -0,0 +1,25 @@
+#MySQL Retake Exam - 30 July 2019
+CREATE SCHEMA `colonial_blog_db`;
+USE `colonial_blog_db`;
+
+#Section 1: Data Definition Language (DDL)
+#01. Table Design
+#Section 2: Data Manipulation Language (DML)
+#02. Insert
+#03. Update
+#04. Delete
+#Section 3: Querying
+#05. Extract 3 biggest articles
+#06. Golden articles
+#07. Extract categories
+#08. Extract the most commented social article
+#09. Extract the less liked comments
+#Section 4: Programmability
+#10. Get users articles count
+#11. Like Article
+
+/*
+ The SoftUni Open Judge System does not accept the
+ DETERMINISTIC and DELIMITER clauses so the functions
+ from Section 4 must be submitted without them.
+ */
